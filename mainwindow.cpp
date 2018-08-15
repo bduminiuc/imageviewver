10df3c690810c28d8c1595fce4d3418c11129e70
bogdanduminyuk <77934329bogdan@gmail.com>
2018-08-15 17:20:38 +0300

    created simple slots for open and close actions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,40 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    initInterface();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::open()
+{
+    close();
+
+    // open directory path
+    QString directoryPath = QFileDialog::getExistingDirectory(nullptr, "Directory Dialog", "");
+
+    ui->statusBar->showMessage("Directory " + directoryPath + " is opened");
+}
+
+void MainWindow::close()
+{
+    ui->statusBar->showMessage("Closed!");
+}
+
+void MainWindow::initInterface()
+{
+    this->showMaximized();
+
+    // create connections between actions and slots
+    connect(ui->action_open, &QAction::triggered, this, &MainWindow::open);
+    connect(ui->action_close, &QAction::triggered, this, &MainWindow::close);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,13 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void open();
+    void close();
+
 private:
     Ui::MainWindow *ui;
+    void initInterface();
 };
 
 #endif // MAINWINDOW_H
