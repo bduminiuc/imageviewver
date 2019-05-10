9ca84dcd604441018423efd5c4b5eefc5bfeac10
bduminyuk <77934329bogdan@gmail.com>
2019-05-10 14:11:23 +0400

    operations with filesystem moved to model/filesystemmanager

--- model/filesystemmanager.cpp
+++ model/filesystemmanager.cpp
@@ -1,6 +1,62 @@
 #include "filesystemmanager.h"
 
-FileSystemManager::FileSystemManager(QObject *parent) : QObject(parent)
-{
+#include <QDirIterator>
+#include <QFileInfo>
 
+
+FileSystemManager::FileSystemManager(QObject *parent) :
+    QObject(parent),
+    mFormats({"*.jpg", "*.png", "*.bmp", "*.ico"})
+{
+    connect(this, &FileSystemManager::rescan, this, &FileSystemManager::scanDirectory);
+}
+
+void FileSystemManager::setScanFormats(const QStringList &formats)
+{
+    mFormats = formats;
+}
+
+void FileSystemManager::copyFiles(const QStringList &srcFiles, const QString &dstDir)
+{
+    for(auto oldPath: srcFiles) {
+        QString newPath = getNewFilePath(oldPath, dstDir);
+        QFile::copy(oldPath, newPath);
+    }
+}
+
+void FileSystemManager::moveFiles(const QStringList &srcFiles, const QString &dstDir)
+{
+    for(auto oldPath: srcFiles) {
+        QString newPath = getNewFilePath(oldPath, dstDir);
+        QFile::rename(oldPath, newPath);
+    }
+}
+
+void FileSystemManager::removeFiles(const QStringList &files)
+{
+    for(auto path: files) {
+        QFile::remove(path);
+    }
+}
+
+void FileSystemManager::scanDirectory(const QString &dir)
+{
+    QDirIterator it(dir, mFormats, QDir::Files, QDirIterator::Subdirectories);
+    ImageList files;
+
+    if (it.hasNext()) {
+        mScannedDir = dir;
+    }
+
+    while(it.hasNext()) {
+        files.append(it.next());
+    }
+
+    emit scanFinished(files);
+}
+
+QString FileSystemManager::getNewFilePath(const QString &oldPath, const QString &newDir)
+{
+    QFileInfo fileinfo(oldPath);
+    return newDir + QDir::separator() + fileinfo.fileName();
 }
--- model/filesystemmanager.h
+++ model/filesystemmanager.h
@@ -3,15 +3,32 @@
 
 #include <QObject>
 
+#include "imagelist.h"
+
 class FileSystemManager : public QObject
 {
     Q_OBJECT
 public:
     explicit FileSystemManager(QObject *parent = nullptr);
 
+    void setScanFormats(const QStringList &formats);
+
+    void copyFiles(const QStringList &srcFiles, const QString &dstDir);
+    void moveFiles(const QStringList &srcFiles, const QString &dstDir);
+    void removeFiles(const QStringList &files);
+
 signals:
+    void rescan(const QString &dir);
+    void scanFinished(const ImageList &files);
 
 public slots:
+    void scanDirectory(const QString &dir);
+
+private:
+    QString getNewFilePath(const QString &oldPath, const QString &newDir);
+
+    QString mScannedDir;
+    QStringList mFormats;
 };
 
 #endif // FILESYSTEMMANAGER_H
