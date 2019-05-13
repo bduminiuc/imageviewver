1e1bb0624cb99635173c06f5a321e0d57a619661
bduminyuk <77934329bogdan@gmail.com>
2019-05-13 22:09:03 +0400

    main.cpp is written to show open dir

--- model/main.cpp
+++ model/main.cpp
@@ -1,8 +1,17 @@
 #include <QCoreApplication>
 
+#include "directory.h"
+#include <iostream>
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    Directory directory({"*.png", "*.jpg", "*.ico"});
+
+    directory.open("C:/Users/host/Documents/imageviewer/images");
+
+    std::cout << directory.count() << std::endl;
 
     return a.exec();
 }
